6b3cd7f47d2ead17476ca636974489b4e618ba7d
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-06-14 23:24:42 +0200

    Redirect ot loginafter registration

--- Frontend/src/pages/Registration.jsx
+++ Frontend/src/pages/Registration.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
 
 function Registration() {
-  return <div>Registration</div>;
+  const [hasRegistered, setHasRegistered] = useState(false);
+
+  const register = async () => {
+    // POST request to api/registration
+    setHasRegistered(true);
+  };
+
+  if (hasRegistered) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div>
+      Registration
+      <button onClick={register}>Register</button>
+    </div>
+  );
 }
 
 export default Registration;
