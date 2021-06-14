dedaba65cff0c19e0a29860e7bcd6681b06ea52d
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-06-14 23:46:05 +0200

    Add navigation logic to home

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 function Home() {
-  return <div>Home</div>;
+  const history = useHistory();
+
+  return (
+    <div>
+      Home
+      <button onClick={() => history.push("/login")}>login</button>
+      <button onClick={() => history.push("/registration")}>register</button>
+    </div>
+  );
 }
 
 export default Home;
