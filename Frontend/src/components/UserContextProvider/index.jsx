057b025f44e874d6e6de781cbdfcbaca845595ce
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-06-14 23:14:23 +0200

    Provide login function through usercontext

--- Frontend/src/components/UserContextProvider/index.jsx
+++ Frontend/src/components/UserContextProvider/index.jsx
@@ -4,8 +4,13 @@ import { UserContext } from "../../context";
 function UserContextProvider({ children }) {
   const [user, setUser] = useState();
 
+  const login = async (email, password) => {
+    // TODO: POST request to api login
+    setUser(true);
+  };
+
   return (
-    <UserContext.Provider value={[user, setUser]}>
+    <UserContext.Provider value={[user, login]}>
       {children}
     </UserContext.Provider>
   );
