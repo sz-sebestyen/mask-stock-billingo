850f222a480caa4b1ea8045714affa7964cab679
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-06-15 20:48:44 +0200

    Use axios

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../context";
 import { useHistory, useLocation } from "react-router-dom";
 import Email from "../components/Input/Email";
 import Password from "../components/Input/Password";
+import axios from "axios";
 
 function Login() {
   let history = useHistory();
@@ -10,17 +11,32 @@ function Login() {
   const [, setUser] = useContext(UserContext);
 
   const { from } = location.state || { from: { pathname: "/" } };
+
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
   const login = () => {
-    // TODO: POST req to /login
-    setUser(true);
-    history.replace(from);
+    axios({
+      method: "POST",
+      data: {
+        username: loginUsername,
+        password: loginPassword,
+      },
+      withCredentials: true,
+      url: "/login",
+    }).then((res) => {
+      console.log(res);
+      setUser(true);
+      history.replace(from);
+    });
+    // .finally(() => setLoggedIn(true));
   };
 
   return (
     <div>
       Login
-      <Email />
-      <Password />
+      <Email onChange={({ target: { value } }) => setLoginUsername(value)} />
+      <Password onChange={({ target: { value } }) => setLoginPassword(value)} />
       <button onClick={login}>login</button>
     </div>
   );
