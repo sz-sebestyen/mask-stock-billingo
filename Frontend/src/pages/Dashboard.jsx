4577670a4947a82434cddee6ea4bf0dcdcc0e4d0
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-06-15 10:31:54 +0200

    Add navigation to hospital registration

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import HospitalList from "../components/HospitalList";
+import { useHistory } from "react-router-dom";
 
 function Dashboard() {
+  const history = useHistory();
+
   return (
     <div>
       Dashboard
+      <button onClick={() => history.push("/hospital-registration")}>
+        Add hospital
+      </button>
       <HospitalList />
     </div>
   );
