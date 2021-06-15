e9c7b971bd013a48e6e4322487b272b145769d8d
Gabor Szabo <gabor.szabo86@hotmail.com>
2021-06-15 10:37:15 +0200

    Add node-fetch, add get partnerts route for test purpose

--- Backend/server.js
+++ Backend/server.js
@@ -7,16 +7,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Database
-mongoose.set('useCreateIndex', true);
-mongoose.set('useFindAndModify', false);
-
-// MongoDb Atlas
-const connectionString = "mongodb+srv://admin:admin@cluster0.wzkbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const connectionString = "mongodb+srv://cluster0.wzkbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(connectionString, { useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
+// Routes
+const billingoApiRoutes = require('./controllers/billingo');
+app.use('/billingoApi', billingoApiRoutes);
 
 app.listen(3001, () => {
     console.log('listening on 3001');
