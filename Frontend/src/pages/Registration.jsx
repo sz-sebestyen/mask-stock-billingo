3bb49667d98abefdc0bc40aa2e2ec0140f66a00a
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-06-15 20:52:40 +0200

    Use axios for registration

--- Frontend/src/pages/Registration.jsx
+++ Frontend/src/pages/Registration.jsx
@@ -3,13 +3,31 @@ import { Redirect } from "react-router-dom";
 import Email from "../components/Input/Email";
 import Password from "../components/Input/Password";
 import PasswordRepeat from "../components/Input/PasswordRepeat";
+import axios from "axios";
 
 function Registration() {
   const [hasRegistered, setHasRegistered] = useState(false);
 
-  const register = async () => {
-    // POST request to api/registration
-    setHasRegistered(true);
+  const [regUsername, setRegUsername] = useState("");
+  const [regPassword, setRegPassword] = useState("");
+  const [regPasswordRep, setRegPasswordRep] = useState("");
+
+  const register = () => {
+    if (regPassword !== regPasswordRep) return;
+
+    axios({
+      method: "POST",
+      data: {
+        username: regUsername,
+        password: regPassword,
+      },
+      withCredentials: true,
+      url: "/register",
+    }).then((res) => {
+      console.log(res);
+      setHasRegistered(true);
+    });
+    // .finally(() => setShowReg(false));
   };
 
   if (hasRegistered) {
@@ -19,9 +37,11 @@ function Registration() {
   return (
     <div>
       Registration
-      <Email />
-      <Password />
-      <PasswordRepeat />
+      <Email onChange={({ target: { value } }) => setRegUsername(value)} />
+      <Password onChange={({ target: { value } }) => setRegPassword(value)} />
+      <PasswordRepeat
+        onChange={({ target: { value } }) => setRegPasswordRep(value)}
+      />
       <button onClick={register}>Register</button>
     </div>
   );
