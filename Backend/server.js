6253bc9bbf5643d50caabc5aaf3e77de42a0b159
anitavttcs <anita.vertetics@hotmail.com>
2021-06-15 10:43:55 +0200

    login / reg. started

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,47 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+// npm i express body-parser cors mongoose passport passport-local cookie-parser bcryptjs express-session
+// npm i nodemon
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+    origin: "http://localhost:3000",  // react app port
+    credentials: true
+}));
+
+// Passport
+app.use(session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(cookieParser("secretcode"));
+
+app.post("/login", (req, res) => {
+    console.log(req.body);
+});
+
+app.post("/register", (req, res) => {
+    console.log(req.body);
+});
+
+app.get("/user", (req, res) => {
+
+});
 
 // Database
 mongoose.set('useCreateIndex', true);
