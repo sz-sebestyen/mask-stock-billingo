577f816312c59a0356a9144264b7c324b3f8d55d
OwerHub <owerwild@gmail.com>
2021-06-16 06:52:23 +0200

    Routing users, hospital OK

--- Backend/server.js
+++ Backend/server.js
@@ -13,13 +13,13 @@ mongoose.set("useFindAndModify", false);
 
 // MongoDb Atlas
 const connectionString =
-  "mongodb+srv://admin:admin@cluster0.wzkbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://admin:admin@cluster0.wzkbd.mongodb.net/MaskStock?retryWrites=true&w=majority";
 
 const tempConnectionSting =
   "mongodb+srv://TryUser:tryPass@cluster0.bilbc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose
-  .connect(tempConnectionSting, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(connectionString, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
@@ -27,100 +27,63 @@ mongoose
 //const User = require("./models/user");
 //const Hospital = require("./models/hospital");
 //const Product = require("./models/product");
-const TestUser = require("./models/testUser");
 
+
+// Rendelés POST
+// - saját bankszámla
+// - kórház azonosíta
+// - Kórház többi adataánek bekérése a databnase-ről
+// - van-e annyi maszk, mint amennyit kér
+
+
+// függvény => user ID-t kap, visszaadja a kórházakat, amik hozzá tartoznak. 
+// user bejelentkezik GET
+// Visszakapja az a saját adatait és a kórházat
+
+// saját adatbázis, számlázási adatainkkal, és maszkok számával 
+// minden hónap elején 10.000 maszkot hozzáadni 
+
+
+
+
+
+//----- Routingot majd később kiszervezem
+
+
+const hospitalRoutes = require("./routes/hospitalRoute")
+app.use("/hospitals" , hospitalRoutes)
+
+const userRoutes = require("./routes/userRoute")
+app.use("/users" , userRoutes)
+
+
+
+const TestUser = require("./models/testUser");
 const TestHospital = require("./models/testHospital");
 
-console.log(testHospital);
-//----- Routingot majd később kiszervezem
 
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
-// kórház felvétele az adatbázisba
-app.post("/hospitals/addNewHospital", async (req, res) => {
-
-  const doc = new TestHospital({
-    name: req.body.name,
-    email: req.body.email,
-    country: req.body.country,
-    city: req.body.city,
-  });
-
-  const response = await doc.save();
-  res.json(response);
-});
-
-// Kórházak teljes listájának lekérdezése
-  app.get("/hospitals/allOf" , async (req, res) => {
-    const response = await TestHospital.find();
-    res.json(response)
-    })
-
-// kórház  keresése, adatainak lekérdezése
-app.get("/hospitals/searchByName/:search" , async (req, res) => {
-  const response = await TestHospital.findOne({name: new RegExp(req.params.search)});
-  res.json(response)
-  })
-
-// -----kórház adatainak módosítása
-// http://localhost:3001//hospitals/update/search.key.value 
-// post : http://localhost:3001//hospitals/update/LechterMed.country.Italy
-app.post("/hospitals/update/:searchName.:key.:value" , async (req, res) => {
-  const response = await TestHospital.updateOne({name: [req.params.searchName] }, {$set: {[req.params.key] : req.params.value} })
-
-  res.json(response)
-})
-
 // ----USER----
 
 
-// User felvétele az adatbázisba
-// post http://localhost:3001/users/addNewUser
-app.post("/users/addNewUser", async (req, res) => {
-  
-  const doc = new TestUser({
-    name: req.body.name,
-    email: req.body.email,
-    country: req.body.country,
-    sex: req.body.sex,
-  });
-  
-  const response = await doc.save();
-  res.json(response);
-});
-
-// Userek teljes listájának lekérdezése
-// post http://localhost:3001/users/addNewUser
-app.get("/users/allOf" , async (req, res) => {
-  const response = await TestUser.find();
-  res.json(response)
-  })
-
-// user keresése, adatainak lekérdezése
-// http://localhost:3001/users/searchByName/Ödön
-
-app.get("/users/searchByName/:search" , async (req, res) => {
-  const response = await TestUser.findOne({name: new RegExp(req.params.search)});
-  res.json(response)
-  })
-
 // user adatainak módosítása
-
-app.post("/users/update/:searchName.:key.:value" , async (req, res) => {
+// post http://localhost:3001//users/update/Zsíros B. Ödön.email.kolesz@terin.hu
+app.post("/usersweq/update/:searchName.:key.:value" , async (req, res) => {
   const response = await TestUser.updateOne({name: [req.params.searchName] }, {$set: {[req.params.key] : req.params.value} })
 
   res.json(response)
 })
 
 
-// saját adatbázis, benne a maszkok száma
-
+// saját adatbázis, 
+// --benne a maszkok száma
 // maszkok számának módosítása
 
-// számla adatbázis létrehozása
 
+// számla adatbázis létrehozása
 // számla adatbázis lekérdezése
 
 app.listen(3001, () => {
