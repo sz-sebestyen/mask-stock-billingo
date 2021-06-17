3d623a039d99abe1a8c263ced12c82786f82bc24
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-06-17 11:16:46 +0200

    Show hospital data

--- Frontend/src/components/HospitalDataCard/index.jsx
+++ Frontend/src/components/HospitalDataCard/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function HospitalDataCard({ hospital }) {
-  return <div>HospitalDataCard</div>;
+  const Haddress = hospital.address;
+
+  return (
+    <div className="border p-2">
+      <h3>Hospital data:</h3>
+      <div>Name: {hospital.name}</div>
+      <div>
+        Address: {Haddress.country_code}, {Haddress.post_code}, {Haddress.city},{" "}
+        {Haddress.address}
+      </div>
+      <div>Email(s): {hospital.emails.join(", ")}</div>
+      <div>Taxcode: {hospital.taxcode}</div>
+    </div>
+  );
 }
 
 export default HospitalDataCard;
--- Frontend/src/pages/Hospital.jsx
+++ Frontend/src/pages/Hospital.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import HospitalDataCard from "../components/HospitalDataCard";
 import MaskOrderForm from "../components/MaskOrderForm";
 import BackButton from "../components/BackButton";
 import MaskStock from "../components/MaskStock";
 import Menu from "../components/Menu";
+import { UserContext } from "../context";
 
 function Hospital() {
   const { id } = useParams();
+  const [user] = useContext(UserContext);
 
   return (
-    <div>
+    <div className="flex flex-col gap-2 items-center mt-20">
       <Menu />
-      Hospital {id}
-      <HospitalDataCard />
+      <HospitalDataCard
+        hospital={user?.hospitals?.find((hospital) => "" + hospital.id === id)}
+      />
       <MaskOrderForm />
       <MaskStock />
       <BackButton>Back</BackButton>
