dc2367d5b270f48dc731bad82fbfb7e7ce8c25e8
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-06-17 14:20:10 +0200

    Edit stock display

--- Frontend/src/components/MaskStock.jsx
+++ Frontend/src/components/MaskStock.jsx
@@ -1,18 +1,22 @@
 import { useState, useEffect } from "react";
 
 export default function MaskStock() {
-	const [masks, setMasks] = useState(null);
-	const url = "http://localhost:3001/maskNumber";
+  const [masks, setMasks] = useState(null);
+  const url = "http://localhost:3001/maskNumber";
 
-	useEffect(() => {
-		fetch(url)
-			.then(res => res.json())
-			.then(data => setMasks(data))
-	}, []);
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setMasks(data));
+  }, []);
 
-	return masks && (
-		<div>
-			(current stock: {new Intl.NumberFormat("hu-HU").format(masks)} pieces)
-		</div>
-	);
+  return (
+    <div>
+      (current stock:{" "}
+      {masks
+        ? new Intl.NumberFormat("hu-HU").format(masks) + " pieces"
+        : "loading..."}
+      )
+    </div>
+  );
 }
