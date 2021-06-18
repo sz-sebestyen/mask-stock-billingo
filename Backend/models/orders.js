969df2e4ff632ac90b19ebdb090a626edcf001f3
OwerHub <owerwild@gmail.com>
2021-06-18 11:05:46 +0200

    It can order

--- Backend/models/orders.js
+++ Backend/models/orders.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
 
-	partner_id: { type: Number, required: true },
+	partner_id: { type: String, required: false },
 	block_id: { type: Number, required: true },
-	bank_account_id:{ type: String, required: true },
+	bank_account_id:{ type: Number, required: false },
 	type: { type: String, required: true },
 	fulfillment_date: { type: String, required: true },
 	due_date:{ type: String, required: true },
@@ -13,14 +13,14 @@ const orderSchema = new mongoose.Schema({
 	currency: { type: String, required: true },
 	conversion_rate: { type: Number, required: true },
 	electronic: { type: Boolean, required: true },
-	paid:{ type: Boolean, required: true },
+	paid:{ type: Boolean, required: false },
 	items: [
 		{
 			product_id:{ type: String, required: true },
 			quantity:{ type: String, required: true },
 		},
 	], 
-	comment: {type: String, required: true }
+	comment: {type: String, required: false }
 });
 
 module.exports = mongoose.model('Order', orderSchema);
