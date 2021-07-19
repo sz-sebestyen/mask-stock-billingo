186182601d85682e2404925c120ba8255c0917d9
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-07-19 13:50:55 +0200

    Remove duplications and fix sonarlint warnings

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,26 @@
+require("dotenv/config");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const passportConfig = require("./passportConfig");
+
+// models
+const maskNumberChange = require("./maskNumberChange");
+const MyDatas = require("./models/myDatas");
+
+// Routes
+const partnersRoutes = require("./controllers/partners");
+const authRoutes = require("./controllers/auth");
+const productsRoutes = require("./controllers/products");
+const bankaccountsRoutes = require("./controllers/bankAccounts");
+const docBlocksRoute = require("./controllers/documentBlocks");
+const documentRoute = require("./controllers/documents");
 
 const app = express();
-require("dotenv/config"); //.env-hez
 
-/* app.use(express.urlencoded({ extended: true })); */
 app.use(express.json());
 app.use(
   cors({
@@ -17,28 +29,7 @@ app.use(
   })
 );
 
-// Passport
-app.use(
-  session({
-    secret: "secretcode",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
-
 app.use(cookieParser("secretcode"));
-app.use(passport.initialize());
-app.use(passport.session());
-require("./passportConfig")(passport);
-
-app.use(
-  cors({
-    origin: "http://localhost:3000", // react app port
-    credentials: true,
-  })
-);
-
-// Passport
 app.use(
   session({
     secret: "secretcode",
@@ -47,38 +38,24 @@ app.use(
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
   })
 );
-//
-app.use(cookieParser("secretcode"));
+
+// Passport
 app.use(passport.initialize());
 app.use(passport.session());
-require("./passportConfig")(passport);
+passportConfig(passport);
 
-// Database
-mongoose.set("useCreateIndex", true);
-mongoose.set("useFindAndModify", false);
-
-// MongoDb Atlas
+// mongoDb
 const connectionString = process.env.DB_CONNECTION;
 
 mongoose
   .connect(connectionString, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
-
-// Modells import
-const User = require("./models/user");
-const Hospital = require("./models/hospital");
-//const Product = require("./models/product");
-
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
-
-// self-datas
-const MyDatas = require("./models/myDatas");
 
 // set datas to default
 app.get("/setDefault", async (req, res) => {
@@ -104,13 +81,6 @@ app.get("/maskNumber", async (req, res) => {
   res.json(ask[0].number_of_masks);
 });
 
-/* app.get("/setMask/:maskNumber" ,  async (req, res) => {
-  const resp = await maskNumberChange(req.params.maskNumber);
-  res.json(resp)
-}) */
-
-const maskNumberChange = require("./maskNumberChange");
-
 async function ifMonthChange() {
   let date = new Date();
   let currentMonth = date.getMonth();
@@ -131,36 +101,13 @@ async function checkDate() {
   }
 }
 
-checkDate(); // ezt rakjuk be a bejelentkezéshez, akkor minden log -in nél leeeleenőrzi a készletet.
-
-/* 
-
-const hospitalRoutes = require("./routes/hospitalRoute");
-app.use("/hospitals", hospitalRoutes);
-
-const userRoutes = require("./routes/userRoute");
-app.use("/users", userRoutes);
-
-const TestUser = require("./models/testUser");
-const TestHospital = require("./models/testHospital"); */
-
-// Routes
-const partnersRoutes = require("./controllers/partners");
-const authRoutes = require("./controllers/auth");
+checkDate();
 
 app.use("/api", partnersRoutes);
 app.use(authRoutes);
-
-const productsRoutes = require("./controllers/products");
 app.use("/api", productsRoutes);
-
-const bankaccountsRoutes = require("./controllers/bankAccounts");
 app.use("/api", bankaccountsRoutes);
-
-const docBlocksRoute = require("./controllers/documentBlocks");
 app.use("/api", docBlocksRoute);
-
-const documentRoute = require("./controllers/documents");
 app.use("/api", documentRoute);
 
 app.listen(3001, () => {
